38844c5f8150d0f521a0d10eba51fadefa0a61cf
Denis-Turbatu <turbatudenis81@gmail.com>
2024-03-26 19:30:49 +0100

    feat: add logical part for ticket in js and css part

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,8 @@ ageValue = parseInt(age.value);
 console.log(age);
 console.log(ageValue);
 
+const username = document.querySelector("#username").value;
+
 const sendBtn = document.getElementById("submit");
 // debug
 console.log(sendBtn);
@@ -23,12 +25,26 @@ let ticket = kmValue * 0.21;
 let discount = 0;
 let discountApplied = 0;
 let priceDiscounted = 0;
+let promo = 0;
+let carriage = Math.floor(Math.random() * 10 + 1);;
+let cp = Math.floor(Math.random() * 10000 + 1);;
 
 sendBtn.addEventListener("click", function(){
+    //Show ticket
+    const ticketUser = document.querySelector('.ticket-user');
+    ticketUser.classList.remove('hidden');
+    ticketUser.classList.add('show');
+
+    //IF statement
     if (ageValue >= 65) {
         discount = 40;
+        promo = "Offerta Anziani";
     } else if (ageValue < 18) {
         discount = 20;
+        promo = "Offerta Giovani";
+    }else{
+        discount = 0;
+        promo = "Nessun Offerta";
     }
 
     // ********** Output **********
@@ -44,6 +60,17 @@ sendBtn.addEventListener("click", function(){
         console.log("Ci dispiace! Ma non hai uno sconto disponibile");
         // Final price
         priceDiscounted = ticket - discountApplied;
-        console.log("Il prezzo finale è di: " + (ticket).toFixed(2) + "€");
+        console.log("Il prezzo finale è di: " + (priceDiscounted).toFixed(2) + "€");
     }
+
+    // Description Ticket
+    document.getElementById("user").innerHTML = "<span>" + username + "</span>";
+    document.getElementById("promo").innerHTML = "<span>" + promo + "</span>";
+    document.getElementById("carriage").innerHTML =
+      "<span>" + "Vagone N°: " + carriage + "</span>";
+    document.getElementById("cp").innerHTML =
+      "<span>" + "CP: " + cp + "</span>";
+    document.getElementById("price").innerHTML =
+      "<span>" + "Prezzo: " + priceDiscounted.toFixed(2) + "€" + "</span>";
 });
+
