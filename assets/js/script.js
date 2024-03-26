e0aea9b31e66e5ee90f2d5d06cfd3b61107cb40e
Denis-Turbatu <turbatudenis81@gmail.com>
2024-03-26 14:23:03 +0100

    feat: data collection from html

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// ***** Raccolta dati *****
+const username = document.querySelector('#username');
+console.log(username);
+
+const number = document.querySelector("#number");
+console.log(username);
+
